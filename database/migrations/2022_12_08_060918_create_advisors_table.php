c9a13df584e00651b984fe2f651b01ac0c41f413
Hebh Alawam <72646425+HebhAlawam@users.noreply.github.com>
2022-12-11 06:32:23 +0200

    Update 2022_12_08_060918_create_advisors_table.php

--- database/migrations/2022_12_08_060918_create_advisors_table.php
+++ database/migrations/2022_12_08_060918_create_advisors_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('advisors', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
-            $table->string('name');
+            $table->string('team');
             $table->timestamps();
         });
     }
