bd29faf7a412ed931bfadd9a2625cca808edbeff
saraIsmailSE <44667136+saraIsmailSE@users.noreply.github.com>
2023-01-17 22:01:10 +0300

    Revert "Revert "new views""

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,30 @@
-<!doctype html>
-<html lang="fa" dir="rtl">
-
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Osboha') }}</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Tajawal&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css')}}">
 
-    <!-- Styles -->
-    <!-- <link rel="stylesheet" href="{{('assets/css/widgets/chat.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}"> -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/app.rtl.css')}}">
+
+    <link rel="stylesheet" href="{{asset('assets/vendors/iconly/bold.css')}}">
+
     <link rel="stylesheet" href="{{asset('assets/vendors/perfect-scrollbar/perfect-scrollbar.css')}}">
     <link rel="stylesheet" href="{{asset('assets/vendors/bootstrap-icons/bootstrap-icons.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/app.rtl.css')}}">
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.svg')}}" type="image/x-icon">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/toastify/toastify.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/choices.js/choices.min.css')}}" />
-    <link rel="stylesheet" href="{{ asset('assets/css/pages/email.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon.svg')}}" type="image/x-icon">
+
+    
+
 
 </head>
 
@@ -38,38 +35,40 @@ body{
 </style>
 <body>
     <input id="APP_URL" type="hidden" value="{{ url('/')}}">
+    <div id="app">
+        <div id="main" class="layout-horizontal">
+        @include('layouts.header')
+            <div class="content-wrapper container">
 
-    @include('layouts.sidebar')
-    <div id="main" class='layout-navbar' style="overflow-x:hidden !important">
-        @yield('header')
-        <div id="main-content">
-            <div class="page-heading">
-                <div class="page-title">
-                    @yield('page_title')
+                <div class="page-heading">
+                    <h3>@yield('pageHeading')</h3>
+                </div>
+                <div class="page-content">
+                    <section class="row">
+
+                    @yield('content')
+
+                    </section>
                 </div>
 
-                @yield('content')
             </div>
+
+            <footer>
+                <div class="container">
+                    <div class="footer clearfix mb-0 text-muted">
+                        <div class="float-start">
+                            <p>2023 &copy; Osboha180</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
-
-        <!-- Script -->
-        <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
-        <script src="{{asset('assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
-        <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-        <script src="{{asset('assets/vendors/ckeditor/ckeditor.js')}}"></script>
-        {{-- <script>
-            ClassicEditor
-                .create(document.querySelector('#editor'))
-                .catch(error => {
-                    console.error(error);
-                });
-        </script> --}}
-        <script src="{{asset('assets/js/pages/form-element-select.js')}}"></script>
-        <script src="{{asset('assets/js/mazer.js')}}"></script>
-        <script src="{{asset('assets/vendors/choices.js/choices.min.js')}}"></script>
+    </div>
+    <script src="{{asset('assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
 
 
+    <script src="{{asset('assets/js/pages/horizontal-layout.js')}}"></script>
 </body>
 
 </html>
